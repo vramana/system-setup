b9d76914f2056b89221c9294e99b108ab0c54044
Ramana Venkata <vramana@users.noreply.github.com>
2018-06-27 20:13:57 +0530

    Add Clang 6.0

---
+++ clang.sh
@@ -0,0 +1,3 @@
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-6.0 main"
+sudo apt install -y clang-6.0
